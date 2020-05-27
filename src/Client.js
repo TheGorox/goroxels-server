6ca8a2d81f9d45c503d555c571f6e7ee00d31b5c
TheGorox <gnikita.010@gmail.com>
2020-05-27 14:55:20 +0300

    Добавлены фазы и отправка ошибок

--- src/Client.js
+++ src/Client.js
@@ -1,15 +1,34 @@
 let lastId = 0;
 // todo replace it with.. something
 
+const {
+    STRING_OPCODES
+} = require('./protocol')
+
+const CLIENT_STATES = {
+    CANVAS_NOT_CHOSEN: 0,
+    CAPTCHA: 1,
+    READY: 2
+}
+
 class Client{
     constructor(socket){
         this.socket = socket;
+
+        this.canvas = null;
+
+        this.state = 0;
 
         this.id = ++lastId;
     }
 
     send(message){
         this.socket.send(message);
+    }
+
+    sendError(message){
+        const str = JSON.stringify([STRING_OPCODES.error, message]);
+        this.socket.send(str);
     }
 }
 
