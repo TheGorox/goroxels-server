e22f74eba5ad0883cd531bb11333f7a150d31274
TheGorox <gnikita.010@gmail.com>
2020-05-27 14:57:23 +0300

    Набросок БД, а также адаптация под систему канвасов

--- src/index.js
+++ src/index.js
@@ -1,9 +1,26 @@
-const Server = require('./Server.js');
-const ChunkManager = require('./ChunkManager');
+require('dotenv').config();
 
-//const conf
-let chunks = new ChunkManager();
-chunks.loadAll();
+const Server = require('./Server');
+const Canvas = require('./Canvas');
 
-const server = new Server(chunks);
-server.run()
+const config = require('./config');
+
+const {
+    DB_HOST,
+    DB_PORT,
+    DB_USER,
+    DB_PASS
+} = process.env;
+
+const db = require('./db');
+db.init(DB_HOST, DB_PORT, DB_USER, DB_PASS);
+
+const canvases = [];
+config.canvases.forEach((canvas, i) => {
+    canvases.push(new Canvas(
+        i,
+        canvas
+    ))
+})
+
+Server.startServer(config.port, canvases)
