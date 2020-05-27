05dc7b7403528dbee24e0af87df014dde9fc0f57
TheGorox <gnikita.010@gmail.com>
2020-05-27 14:58:57 +0300

    Полностью переделан

--- src/Server.js
+++ src/Server.js
@@ -1,100 +1,31 @@
-const http = require('http');
-const {
-    Server: WebSocketServer
-} = require('ws');
+const logger = require('log4js').getLogger('SERVER');
+logger.level = 'info';
 
-const Client = require('./Client');
-const {
-    OPCODES,
-    createPacket
-} = require('./protocol');
-const {
-    unpackPixel,
-    packPixel
-} = require('./utils');
-const {
-    realBoardWid,
-    realBoardHei,
-    palette
-} = require('./config')
+const express = require('express');
+const https = require('http');
 
-class Server {
-    constructor(cm) {
-        this.chunkManager = cm;
+const Socket = require('./WebsocketServer');
 
-        this.clients = new Map();
-    }
+function startServer(port, canvases){
+    const app = express();
+    const server = https.createServer(app);
 
-    run(config = {}) {
-        const wss = new WebSocketServer({
-            port: config.port || 1488,
-        });
-        wss.on('connection', (socket, request) => {
-            const client = new Client(socket);
+    // TODO handle world names?
+    app.use(/\/[\d\w]{0,32}/, express.static(__dirname + '/../public'));
+    // kostyl↓
+    app.use('/', express.static(__dirname + '/../public'));
 
-            this.clients.set(socket, client);
+    const webSocketServer = new Socket(canvases);
 
-            socket.onclose = () => {
-                this.clients.delete(socket);
-                this.broadcastOnline();
-            }
-
-            socket.onmessage = (event) => this.onmessage(socket, event);
-
-            this.broadcastOnline();
-        })
-
-        console.log('Listening!');
-    }
-
-    onmessage(socket, ev) {
-        // TODO: replace it with more effective
-        // event type check
-        if (ev.type !== 'message') return;
-
-        let message = ev.data;
-        if (typeof message === 'string') {
-            console.log(message)
-        } else {
-            switch (message.readUInt8(0)) {
-                // TODO: check is coords in bounds
-                case OPCODES.chunk: {
-                    const cx = message.readUInt8(1);
-                    const cy = message.readUInt8(2);
-
-                    let chunkData = this.chunkManager.getChunkData(cx, cy);
-
-                    socket.send(createPacket.chunkSend(cx, cy, chunkData));
-
-                    break
-                }
-                case OPCODES.place: {
-                    const [x, y, c] = unpackPixel(message.readUInt32BE(1));
-
-                    if (x < 0 || x >= realBoardWid ||
-                        y < 0 || y >= realBoardHei ||
-                        c < 0 || c >= palette.length) return;
-
-                    this.chunkManager.setChunkPixel(x, y, c);
-                    this.broadcast(message)
-                }
-            }
-        }
-    }
-
-    broadcastOnline(){
-        let online = this.clients.size;
-
-        let buf = Buffer.allocUnsafe(1 + 3);
-        buf.writeUInt8(OPCODES.online, 0);
-        buf.writeUInt16BE(online, 1);
-
-        this.broadcast(buf);
-    }
-
-    broadcast(message) {
-        this.clients.forEach(client => client.send(message));
-    }
+    logger.info('Listening http + websocket on port ' + port);
+    server.listen(port);
+    webSocketServer.run(server);
 }
 
-module.exports = Server
+// const compression = require('compression');
+
+// app.use(compression());
+
+module.exports = {
+    startServer
+}
