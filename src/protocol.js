ac4faef24b66831f4969572bce3f9eb09b2e9674
TheGorox <gnikita.010@gmail.com>
2020-05-27 14:58:29 +0300

    Добавлен ОПкод канваса и ошибки

--- src/protocol.js
+++ src/protocol.js
@@ -5,11 +5,12 @@ const {
 const OPCODES = {
     chunk: 0x0,
     place: 0x1,
-    online: 0x2
+    online: 0x2,
+    canvas: 0x3
 }
 
 const STRING_OPCODES = {
-
+    error: 'e'
 }
 
 const createPacket = {
