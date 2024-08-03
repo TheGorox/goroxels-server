da40e94a84b1b8d30daa7fbfe7b8c83765cd51b1
Gorox <gnikita.010@gmail.com>
2024-08-03 17:42:05 +0300

    migration fix column name

--- src/db/migrations/20221127233138-user-add-fb-id.js
+++ src/db/migrations/20221127233138-user-add-fb-id.js
@@ -10,6 +10,6 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Users', 'dbId');
+    await queryInterface.removeColumn('Users', 'fbId');
   }
 };
