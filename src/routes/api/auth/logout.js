f81047f241c20933046f66a81de157ba05acfb53
Gorox <gnikita.010@gmail.com>
2024-08-03 17:43:25 +0300

    added error handling to logout

--- src/routes/api/auth/logout.js
+++ src/routes/api/auth/logout.js
@@ -1,9 +1,17 @@
+const logger = require('../../../logger')('API', 'debug');
+
 module.exports = (req, res) => {
     if(req.user){
-        req.logout();
-        res.json({
-            success: true
-        })
+        req.logout(err => {
+            if(err){
+                logger.error(err);
+                return res.error('Internal server error on logout');
+            }
+
+            res.json({
+                success: true
+            })
+        });
     }else{
         res.error('You are not logged in!');
     }
